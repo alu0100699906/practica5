db3f82ed55c39bc4fd3c87b3647273bc9520ed1d
Leonor Priego <alu0100699906@ull.edu.es>
2013-10-18 15:41:02 +0100

    agregado 1 assert y 1 failure

--- tc_racional.rb
+++ tc_racional.rb
@@ -4,5 +4,17 @@ require "./racional.rb"
 require "test/unit"
 
 class Test_Fraccion < Test::Unit::TestCase
+ def setup
+    @p1 = Fraccion.new(1,3)
+    @p2 = Fraccion.new(1,4)
+    @p3 = Fraccion.new(-4,5)
+  end
 
+
+  def test_failure
+    
+    assert_equal("3 / 4",@p1.suma(@p2).to_s)
+    
+  end
+    
 end
